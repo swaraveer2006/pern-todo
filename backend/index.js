edfa23580adf86975fe82bfb65e040c9eb6a5b08
swaraveer2006 <swaraveer2006@gmail.com>
2026-08-19 17:05:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ const PORT = process.env.port || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:"https://frontend-5oip.onrender.com"
+}
+    
+));
 app.use(express.json());
 
 app.use("/todos",todoRoutes);
